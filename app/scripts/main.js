a2419f9d8feda5c1c7df6669192b4fa08be3a59f
Vinny <vdicairano91@gmail.com>
2019-07-22 17:40:57 -0400

    home page design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+console.log('v i n n y');
+
+
+document.getElementById("aboutTrigger").onclick = function(){
+  document.getElementById("mainPhoto").classList.add("hidden");
+  document.getElementById("introText").classList.add("hidden");
+}
