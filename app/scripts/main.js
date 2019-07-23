9d91ae4ce5323a3415ed0baffc1907f26ccf583a
Vinny <vdicairano91@gmail.com>
2019-07-23 17:08:11 -0400

    work page set up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,4 +4,13 @@ console.log('v i n n y');
 document.getElementById("aboutTrigger").onclick = function(){
   document.getElementById("mainPhoto").classList.add("hidden");
   document.getElementById("introText").classList.add("hidden");
+  
+  document.getElementById("aboutText").classList.remove("hidden");
+}
+
+
+document.getElementById("workTrigger").onclick = function(){
+  document.getElementById("mainPhoto").classList.add("hidden");
+  document.getElementById("introText").classList.add("hidden");
+  document.getElementById("workContainer").classList.remove("hidden");
 }
